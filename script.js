74eec26859f9079f87e3f3eb6cca1f67fbfa9212
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-08-16 19:29:45 +0100

    Add book constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, numOfPages, readBook) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.readBook = readBook;
+}
+
+function addBookToLibrary(book) {
+  
+}
