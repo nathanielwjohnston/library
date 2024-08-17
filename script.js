b992c0ffe2bc1594f1f233541d4f6ca4587d3a0a
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-08-17 22:07:48 +0100

    Add basic book display function

--- script.js
+++ script.js
@@ -13,5 +13,39 @@ function addBookToLibrary(book) {
   }
 }
 
+function displayLibrary() {
+  myLibrary.forEach(book => {
+    let newDiv = document.createElement("Div");
+
+    let title = document.createElement("H3");
+    title.appendChild(document.createTextNode(book.title));
+
+    let author = document.createElement("Div");
+    author.appendChild(document.createTextNode(book.author));
+
+    let numOfPages = document.createElement("Div");
+    numOfPages.appendChild(document.createTextNode(book.numOfPages)) 
+
+    let readBook = document.createElement("INPUT");
+    readBook.setAttribute("type", "radio");
+    
+    if (book.readBook) {
+      readBook.checked = true;
+    } else {
+      readBook.checked = false;
+    }
+
+    let divChildren = [title, author, numOfPages, readBook];
+
+    for (const child of divChildren) {
+      newDiv.appendChild(child);
+    }
+
+    let bookContainer = document.querySelector(".books-container");
+    bookContainer.appendChild(newDiv);
+
+  })
+}
+
 const book1 = new Book("Eragon", "Christopher Paolini", 544, true);
 const book2 = new Book("Atomic Habits", "James Clear", 320, true);
